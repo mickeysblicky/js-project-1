a651c7541b1044a8f3e254400d96b195935353ff
Michael Moreno <micmoreno11@gmail.com>
2022-03-11 15:42:27 -0800

    Updated to 5 rounds includes new score count and who won

--- js-project-1.js
+++ js-project-1.js
@@ -2,7 +2,7 @@ const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
 
-// computer #1
+// computer
 function computerPlay(objects) {
     const randomIndex = Math.floor(Math.random() * objects.length);
     const item = objects[randomIndex];
@@ -10,23 +10,17 @@ function computerPlay(objects) {
 }
 
 const array = [rock, paper, scissors];
-const result = computerPlay(array);
-console.log(result);
 
+let playerWin = 0;
+let computerWin = 0;
 
-//player
-
-const answer = prompt("What do you choose? Rock, paper, or scissors?");
-
-
-
-function gameRound2(playerSelection, computerSelection) {
+function gameRound(playerSelection, computerSelection,) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection === scissors) {
-        return ('You win! Rock beats scissors');
+        return ('You win! Rock beats scissors.');
     } else if (playerSelection.toLowerCase() =="rock" && computerSelection === paper) {
-        return ('You lose. Paper beats rock');
+        return ('You lose. Paper beats rock.');
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection === rock) {
-        return ('You lose. Rock beats scissors');
+        return ('You lose. Rock beats scissors.');
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection === paper) {
         return ('You win!. Scissors beats paper.');
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection === rock) {
@@ -42,10 +36,54 @@ function gameRound2(playerSelection, computerSelection) {
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection === scissors) {
         return ('Draw. No one wins.');
     }
-    
 }
 
-const playerSelection = answer;
-console.log(playerSelection);
-const computerSelection = result;
-console.log(gameRound2(playerSelection, computerSelection));
+function win(playerSelection, computerSelection,) {
+    if (playerSelection.toLowerCase() == "rock" && computerSelection === scissors) {
+        playerWin++;
+    } else if (playerSelection.toLowerCase() =="rock" && computerSelection === paper) {
+        computerWin++;
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection === rock) {
+        computerWin++;
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection === paper) {
+        playerWin++;
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection === rock) {
+        playerWin++;
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection === scissors) {
+        computerWin++;
+    }
+    
+    if (playerSelection.toLowerCase() == "rock" && computerSelection === rock) {
+        
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == paper) {
+        
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection === scissors) {
+        
+    }
+}
+    
+game();
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const computerAnswer = computerPlay(array);
+        console.log(computerAnswer);
+        const playerAnswer = prompt("What do you choose? Rock, paper, or scissors?");
+        console.log(playerAnswer);
+        console.log(gameRound(playerAnswer, computerAnswer));
+        win(playerAnswer, computerAnswer);
+        console.log('Player Score: ' + playerWin + ' Computer Score: ' + computerWin );
+        
+        if (playerWin === 3) {
+            console.log('Game over! You won the game!');
+            console.log('Final Score: Player : ' + playerWin + ' Computer: ' + computerWin);
+            break;
+        } else if (computerWin === 3) {
+            console.log('Game over. The computer won the game.')
+            console.log('Final Score: Player : ' + playerWin + ' Computer: ' + computerWin);
+            break;
+        } else if (playerWin === 2 && computerWin === 2) {
+            console.log('Draw. Nobody won.');
+        }
+    }
+}
